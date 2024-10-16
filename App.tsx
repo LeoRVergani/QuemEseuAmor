659eb63a5fbd893452e2bfb6d8fe73259f746093
leorvergani <vergani@outlook.com.br>
2024-10-16 03:11:11 -0300

    Inserido Modal

--- App.tsx
+++ App.tsx
@@ -1,11 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, Image, ImageBackground, Modal, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  
+  const [modalActive, SetModalActive] = useState(false)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Modal 
+        visible={modalActive}
+        transparent={true}
+        animationType='fade'
+        onRequestClose={() => SetModalActive(false)}
+        >
+          <View style={styles.outView}>
+              <View style={styles.modalView}>
+                <Text style={styles.textModal}>Essa pessoa está apaixonada por você.</Text>
+                <Image style={styles.juao} source={require('./assets/juao.png')}/>
+              </View>
+          </View>
+
+      </Modal>
+
+        <ImageBackground source={require('./assets/bd.png')} resizeMode='cover' style={styles.image}>
+      
+
+        <View style={styles.card}>
+
+          <Text style={styles.text}>Quer encontrar seu amor?</Text>
+
+            <View style={styles.button}>
+              <Button title='Clique aqui' color={'#db2140'} onPress={() => SetModalActive(true)}/>
+            </View>
+
+
+
+          </View>  
+
+
+        
+        
+        <StatusBar style="auto" />
+        </ImageBackground>
     </View>
   );
 }
@@ -17,4 +54,54 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+    width: '100%',
+    alignItems: 'center'
+  },
+  card: {
+    width: '80%',
+    backgroundColor: 'white',
+    height: 250,
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: '#CCC',
+    borderRadius: 15,
+    alignContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#d87586'
+  },
+  button: {
+    paddingTop: 25,
+    justifyContent: 'center',
+    width: 140
+  },
+  outView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.2)'
+  },
+  modalView: {
+    backgroundColor: 'white',
+    borderRadius: 8,
+    padding: 35,
+    width: 300,
+    alignItems: 'center'
+  },
+  juao: {
+    width: 250,
+    height: 250,
+    borderRadius: 20
+  },
+  textModal: {
+    fontSize: 25,
+    paddingBottom: 10,
+    textAlign: 'center'
+  }
 });
